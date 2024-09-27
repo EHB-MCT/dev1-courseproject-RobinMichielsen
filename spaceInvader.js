952f12b660c8cc646301f557afd6388ffac7f3fc
Robin Michielsen <robin.michielsen@student.ehb.be>
2024-09-27 21:38:10 +0200

    signature created

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,21 @@
-"use strict";
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
 
-alert("Draw your space invader here");
+
+signature()
+
+function signature() {
+
+    context.fillRect(100, 100, 300, 300);
+    context.fillStyle = "orange";
+    context.fillRect(125, 325, 250, 50);  
+    context.fillRect(125, 175, 50, 50);
+    context.fillRect(175, 175, 50, 50);
+    context.fillRect(175, 125, 50, 50);
+    context.fillRect(225, 125, 50, 50);
+    context.fillRect(275, 125, 50, 50);
+    context.fillRect(275, 175, 50, 50);
+    context.fillRect(325, 175, 50, 50);
+}
