e3d23409381ffbdd714eeaf5328986d31eabc33e
Robin Michielsen <robin.michielsen@student.ehb.be>
2024-11-12 21:25:19 +0100

    changed my idea

--- generativeDesign.js
+++ generativeDesign.js
@@ -1,40 +1,47 @@
-import context from "./scripts/context.js"
+import context from "./scripts/context.js";
 import * as utils from "./scripts/utils.js";
 
-let width = context.canvas.width;
-let height = context.canvas.height;
 
-drawPattern();
+const width = context.canvas.width;
+const height = context.canvas.height;
 
-function drawPattern(){
-    drawRectangles(width / 6.5, height / 6, width / 1.4, height / 1.4);
-    drawLines(width / 6, height / 4, width / 2 * 1.666, height / 4);
-    drawCrackedLines();
-}
+context.fillStyle = "black";
+context.fillRect(0,0,width,height);
+
+let size = 20;
+let color;
+
+drawCircle(width/2, height/2, size, color);
+drawRectangles(width/2 - 25, height/2 + 30);
+drawRectangles2(width/2 - 80, height/2 - 25);
+
+function drawCircle(x, y, size, color) {
+        color = "red";
+        context.fillStyle = color;
+        utils.fillCircle(x,y,size);
+} 
 
 
-function drawRectangles(x, y, width, height) {
-context.strokeStyle = "black";
-context.strokeRect(x, y, width, height);
-}
+function drawRectangles(x, y) {
 
-function drawLines(x1, y1, x2, y2) {
-    utils.drawLine(x1, y1, x2, y2);
-}
 
-function drawCrackedLines() {
-    context.strokeStyle = "black";
-    context.lineWidth = 1;
-    utils.drawLine(255,600, 350, 550);
-    utils.drawLine(355,550, 450, 600);
-    utils.drawLine(455,600, 550, 550);
-    utils.drawLine(555,550, 650, 600);
-    utils.drawLine(655,600, 750, 550);
-    utils.drawLine(755,550, 850, 600);
-    utils.drawLine(855,600, 950, 550);
-    utils.drawLine(955,550, 1050, 600);
-    utils.drawLine(1055,600, 1150, 550);
-    utils.drawLine(1155,550, 1250, 600);
+    for (let i = 0; i < 50; i++) {
+        color = "yellow";
+        context.fillStyle = color;
+        context.fillRect(x + i * utils.randomNumber(-5, 5),y + i * utils.randomNumber(5, 10),50,50);
 
-}
+    }
 
+} 
+
+function drawRectangles2(x, y) {
+
+
+    for (let i = 0; i < 50; i++) {
+        color = utils.rgb();
+        context.fillStyle = color;
+        context.fillRect(x + i * utils.randomNumber(-25, -20),y + i * utils.randomNumber(-5, 5),50,50);
+
+    }
+
+} 
