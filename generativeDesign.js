4cea67c99f60340193e853e6901f189110740a91
Robin Michielsen <robin.michielsen@student.ehb.be>
2024-11-12 23:24:26 +0100

    made a cool effect with random rectangles and colors

--- generativeDesign.js
+++ generativeDesign.js
@@ -8,15 +8,17 @@ const height = context.canvas.height;
 context.fillStyle = "black";
 context.fillRect(0,0,width,height);
 
-let size = 20;
+let size = 32;
 let color;
 
-drawCircle(width/2, height/2, size, color);
+drawCircle(width/2 - 2, height/2 - 4, size, color);
 drawRectangles(width/2 - 25, height/2 + 30);
 drawRectangles2(width/2 - 80, height/2 - 25);
+drawRectangles3(width/2 - 25, height/2 - 85);
+drawRectangles4(width/2 + 30, height/2 - 25);
 
 function drawCircle(x, y, size, color) {
-        color = "red";
+        color = utils.rgb(utils.randomNumber(0,255), utils.randomNumber(0,255), utils.randomNumber(0,255))
         context.fillStyle = color;
         utils.fillCircle(x,y,size);
 } 
@@ -25,10 +27,10 @@ function drawCircle(x, y, size, color) {
 function drawRectangles(x, y) {
 
 
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 70; i++) {
         color = "yellow";
         context.fillStyle = color;
-        context.fillRect(x + i * utils.randomNumber(-5, 5),y + i * utils.randomNumber(5, 10),50,50);
+        context.fillRect(x + i * utils.randomNumber(-5, 5),y + i * utils.randomNumber(5, 10),45,45);
 
     }
 
@@ -37,10 +39,34 @@ function drawRectangles(x, y) {
 function drawRectangles2(x, y) {
 
 
-    for (let i = 0; i < 50; i++) {
-        color = utils.rgb();
+    for (let i = 0; i < 70; i++) {
+        color = "blue";
         context.fillStyle = color;
-        context.fillRect(x + i * utils.randomNumber(-25, -20),y + i * utils.randomNumber(-5, 5),50,50);
+        context.fillRect(x + i * utils.randomNumber(-25, -20),y + i * utils.randomNumber(-5, 5),45,45);
+
+    }
+
+} 
+
+function drawRectangles3(x, y) {
+
+
+    for (let i = 0; i < 70; i++) {
+        color = "green";
+        context.fillStyle = color;
+        context.fillRect(x + i * utils.randomNumber(-10, 9),y + i * utils.randomNumber(-8, -10),45,45);
+
+    }
+
+} 
+
+function drawRectangles4(x, y) {
+
+
+    for (let i = 0; i < 70; i++) {
+        color = "red";
+        context.fillStyle = color;
+        context.fillRect(x + i * utils.randomNumber(25, 20),y + i * utils.randomNumber(-5, 5),45,45);
 
     }
 
