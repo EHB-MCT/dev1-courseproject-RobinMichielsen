adcc598a6d735c11672eb7ec9a8bd2b440be278e
Robin Michielsen <robin.michielsen@student.ehb.be>
2024-12-19 03:26:13 +0100

    changed the program

--- generativeDesign.js
+++ generativeDesign.js
@@ -4,82 +4,72 @@ import * as utils from "./scripts/utils.js";
 const width = context.canvas.width;
 const height = context.canvas.height;
 
-    // de positie van de cursor opvragen
-
-window.onmousemove = move;
-
-/**
- * 
- * @param {MouseEvent} eventData 
- */
-
-function move(eventData) {
-    let x = eventData.pageX;
-    let y = eventData.pageY;
-    
-
-
-    // achtergrond zwart maken na elke beweging opnieuw
-    context.fillStyle = "black";
-    context.fillRect(0,0,width,height);
-    // de functies oproepen met de positie in width en height om ervoor te zorgen dat het design correct getekend word op alle schermformaten
-    drawCircle(x, y, size, color);
-    drawRectanglesDown(x - 25, y + 30);
-    drawRectanglesLeft(x - 80, y - 25);
-    drawRectanglesUp(x - 25, y - 85);
-    drawRectanglesRight(x + 30, y - 25);
-    
-}
-
 //variabelen aanmaken 
-let size = 32;
 let color;
 let squareSize = [45, 55, 65, 75];
+let xPos = width / 2;
+let yPos = height / 2;
+let frameCount = 0;
 
-// de functie om de cirkel in het midden te tekenen
-function drawCircle(x, y, size, color) {
-        color = utils.rgb(utils.randomNumber(150,255), utils.randomNumber(150,255), utils.randomNumber(150,255));
-        context.fillStyle = color;
-        utils.fillCircle(x,y,size);
+// achtergrond zwart maken na elke beweging opnieuw
+context.fillStyle = "black";
+context.fillRect(0,0,width,height);
+// de functies oproepen met de positie in width en height om ervoor te zorgen dat het design correct getekend word op alle schermformaten
+update();
+
     
+// de functie om de cirkel in het midden te tekenen
+function update() {
+    frameCount++;
+    context.fillStyle = utils.rgb(utils.randomNumber(150,255), utils.randomNumber(150,255), utils.randomNumber(150,255));
+    utils.fillCircle(xPos,yPos, 32);
+    xPos += utils.randomNumber(-10, 10);
+    context.fillStyle = "black";
+    context.fillRect(0, 0, width, height);
+    requestAnimationFrame(update);
+    drawRectanglesDown(xPos - 25, yPos + 30);
+    drawRectanglesLeft(xPos - 80, yPos - 25);
+    drawRectanglesUp(xPos - 25, yPos - 85);
+    drawRectanglesRight(xPos + 30, yPos - 25);
+
 } 
 
 // de functie om de rechthoeken naar beneden te tekenen
-function drawRectanglesDown(x, y) {
+function drawRectanglesDown(xPos, yPos) {
+    
     for (let i = 0; i < 70; i++) {
         color = utils.rgb(utils.randomNumber(0,100), utils.randomNumber(0,100), utils.randomNumber(0,100));
         context.fillStyle = color;
-        context.fillRect(x + i * utils.randomNumber(-5, 5),y + i * utils.randomNumber(5, 10),squareSize[0],squareSize[0]);
-       
+        context.fillRect(xPos + i * utils.randomNumber(-5, 5),yPos + i * utils.randomNumber(5, 10),squareSize[0],squareSize[0]);
     }
 } 
 
 // de functie om de rechthoeken naar links te tekenen
-function drawRectanglesLeft(x, y) {
+function drawRectanglesLeft(xPos, yPos) {
     for (let i = 0; i < 70; i++) {
         color = utils.rgb(utils.randomNumber(0,100), utils.randomNumber(0,100), utils.randomNumber(0,100));
         context.fillStyle = color;
-        context.fillRect(x + i * utils.randomNumber(-25, -20),y + i * utils.randomNumber(-5, 5),squareSize[0],squareSize[0]);
+        context.fillRect(xPos + i * utils.randomNumber(-25, -20),yPos + i * utils.randomNumber(-5, 5),squareSize[0],squareSize[0]);
     }
 } 
 
 // de functie om de rechthoeken naar boven te tekenen
-function drawRectanglesUp(x, y) {
+function drawRectanglesUp(xPos, yPos) {
     for (let i = 0; i < 70; i++) {
         color = utils.rgb(utils.randomNumber(0,100), utils.randomNumber(0,100), utils.randomNumber(0,100));
         context.fillStyle = color;
-        context.fillRect(x + i * utils.randomNumber(-10, 9),y + i * utils.randomNumber(-8, -10),squareSize[0],squareSize[0]);
+        context.fillRect(xPos + i * utils.randomNumber(-10, 9),yPos + i * utils.randomNumber(-8, -10),squareSize[0],squareSize[0]);
 
     }
 
 } 
 
 // de functie om de rechthoeken naar rechts te tekenen
-function drawRectanglesRight(x, y) {
+function drawRectanglesRight(xPos, yPos) {
     for (let i = 0; i < 70; i++) {
         color = utils.rgb(utils.randomNumber(0,100), utils.randomNumber(0,100), utils.randomNumber(0,100));
         context.fillStyle = color;
-        context.fillRect(x + i * utils.randomNumber(25, 20),y + i * utils.randomNumber(-5, 5),squareSize[0],squareSize[0]);
+        context.fillRect(xPos + i * utils.randomNumber(25, 20),yPos + i * utils.randomNumber(-5, 5),squareSize[0],squareSize[0]);
     }
 } 
 
